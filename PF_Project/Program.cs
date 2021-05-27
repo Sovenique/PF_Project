c37dbf5e8b2146ec899bd3dd103e0a159d151769
vasilas.msi <vasilas.cei@gmail.com>
2021-05-27 20:28:26 +0300

    Add project files.

---
+++ PF_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PF_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
